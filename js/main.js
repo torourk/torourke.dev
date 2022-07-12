99fb1e3c43a81158fa138d0d23b2c2118f9ead63
Tiernan O'Rourke <2722069+torourk@users.noreply.github.com>
2022-07-11 23:22:32 -0400

    Refactor dark mode js and main page template

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
-function isDarkMode() {
+function queryDarkMode() {
     let theme_val = localStorage.getItem("theme");
     return (theme_val == "dark") ||
         ((theme_val == null) && window.matchMedia("(prefers-color-scheme: dark)").matches);
 }
 
 function setDarkMode(dark_mode) {
+    // Stores the dark mode setting in local storage.
+    let theme_value = dark_mode ? "dark" : "light";
+    localStorage.setItem("theme", theme_value);
+
+    // Updates CSS and theme.
     if (dark_mode) {
         document.documentElement.classList.add("dark-mode");
         document.querySelector('meta[name="theme-color"]').setAttribute("content", "#121212");
@@ -15,11 +20,10 @@ function setDarkMode(dark_mode) {
 }
 
 function toggleDarkMode() {
-    let dark_mode = !isDarkMode();
-    let theme_value = dark_mode ? "dark" : "light";
-    localStorage.setItem("theme", theme_value);
-    setDarkMode(dark_mode);
+    let dark_mode_enabled = queryDarkMode();
+    setDarkMode(!dark_mode_enabled);
 }
 
-let dark_mode = isDarkMode();
+// Applies preferred theme on load.
+let dark_mode = queryDarkMode();
 setDarkMode(dark_mode);
